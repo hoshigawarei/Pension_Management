bd655b4a4ac6a88a256e38b93e7f8d7aba2725f2
hoshigawarei <151939846+hoshigawarei@users.noreply.github.com>
2025-01-07 01:53:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SECRET_KEY = 'b8f14cb7d7f2749b53e0a67f1b13c4ea4421a779c58cf5d9'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost/pension_management'
+    SECRET_KEY = ''
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://usernamepassword:@localhost/pension_management'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
