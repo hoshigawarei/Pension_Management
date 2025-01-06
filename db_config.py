955d9d804c1f61607864a02a22d512dd70d24573
hoshigawarei <151939846+hoshigawarei@users.noreply.github.com>
2025-01-07 01:55:20 +0800

    Create db_config.py

---
+++ db_config.py
@@ -0,0 +1,8 @@
+# db_config.py
+DB_CONFIG = {      
+    'host': 'localhost',                   
+    'port': 3306,                
+    'user': 'xxx',                  # 修改为MySQL 用户名
+    'password': 'xxx',               # 修改为MySQL 密码
+    'database': 'pension_management'
+}
